95bba57ee836fc7c1d81eacc438e4649d3346d47
Rodrigo <rjaencobos@safareyes.es>
2024-11-04 20:17:19 +0100

    Inicio Proyecto SafeHands

---
+++ src/main/java/com/valencia/proyecto1evaluacion/Proyecto1EvaluacionApplication.java
@@ -0,0 +1,13 @@
+package com.valencia.proyecto1evaluacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto1EvaluacionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Proyecto1EvaluacionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/valencia/proyecto1evaluacion/Proyecto1EvaluacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valencia.proyecto1evaluacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto1EvaluacionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
