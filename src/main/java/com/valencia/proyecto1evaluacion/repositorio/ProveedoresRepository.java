7203fe10ede18231ea2e78922a4de7d8cd585807
davidmato <dmatodelalozapalomo@safareyes.es>
2024-11-08 15:41:27 +0100

    se ha añadido el registro del proveedor y su creacion como rol proveedor y usuario

--- src/main/java/com/valencia/proyecto1evaluacion/repositorio/ProveedoresRepository.java
+++ src/main/java/com/valencia/proyecto1evaluacion/repositorio/ProveedoresRepository.java
@@ -4,8 +4,11 @@ import com.valencia.proyecto1evaluacion.modelos.Proveedores;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface ProveedoresRepository  extends JpaRepository<Proveedores, Integer> {
+    Optional<Proveedores> findByUsuarioId(Integer usuarioId);
 
 }
