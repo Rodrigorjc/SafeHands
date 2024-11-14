3f29296969543db08b30699dc8df09ee860e1ac8
davidmato <dmatodelalozapalomo@safareyes.es>
2024-11-14 15:52:34 +0100

    se ha cambiado el parametro long por intenger en el repositorio, ya que daba fallos a la hora de enlazar foreign keys

--- src/main/java/com/valencia/proyecto1evaluacion/repositorio/ProductoRepository.java
+++ src/main/java/com/valencia/proyecto1evaluacion/repositorio/ProductoRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
 }
