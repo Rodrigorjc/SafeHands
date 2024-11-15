706bcb05912e19d6d17526132b2b2b7383d7c1b1
davidmato <dmatodelalozapalomo@safareyes.es>
2024-11-15 19:40:20 +0100

    creacion ongdto

---
+++ src/main/java/com/valencia/proyecto1evaluacion/dtos/OngDTO.java
@@ -0,0 +1,17 @@
+package com.valencia.proyecto1evaluacion.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OngDTO {
+
+    private Integer numVoluntarios;
+    private String sede;
+    private String ubicacion;
+    private String img;
+    private String descripcion;
+}
