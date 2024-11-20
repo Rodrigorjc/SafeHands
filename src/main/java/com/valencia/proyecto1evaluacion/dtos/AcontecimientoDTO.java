4d36da2bbf4f5a125462677e5ea57a9797524273
davidmato <dmatodelalozapalomo@safareyes.es>
2024-11-20 17:26:23 +0100

    implementacion del idOng en la dto

--- src/main/java/com/valencia/proyecto1evaluacion/dtos/AcontecimientoDTO.java
+++ src/main/java/com/valencia/proyecto1evaluacion/dtos/AcontecimientoDTO.java
@@ -1,6 +1,7 @@
 package com.valencia.proyecto1evaluacion.dtos;
 
 
+import com.valencia.proyecto1evaluacion.modelos.Ong;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,4 +15,5 @@ public class AcontecimientoDTO {
     private String descripcion;
     private String ubicacion;
     private String img;
+    private Ong idOng;
 }
