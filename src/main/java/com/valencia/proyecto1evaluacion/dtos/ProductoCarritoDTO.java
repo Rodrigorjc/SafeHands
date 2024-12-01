d33eaa8dd357e23a9dd03e3e4d6c56e1d033c0a4
Rodrigo <rjaencobos@safareyes.es>
2024-12-01 18:50:35 +0100

    ProductoCarritoDTO creado.

---
+++ src/main/java/com/valencia/proyecto1evaluacion/dtos/ProductoCarritoDTO.java
@@ -0,0 +1,19 @@
+package com.valencia.proyecto1evaluacion.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductoCarritoDTO {
+
+    private Integer idProducto;
+    private Integer cantidad;
+    private Double precioUnitario;
+    private Double total;
+
+}
