067d62915b92635303991eba04fd704419dbf93b
Rodrigo <rjaencobos@safareyes.es>
2024-12-01 18:46:53 +0100

    Consulta comentada dado que daba errores.

--- src/main/java/com/valencia/proyecto1evaluacion/repositorio/ClienteRepository.java
+++ src/main/java/com/valencia/proyecto1evaluacion/repositorio/ClienteRepository.java
@@ -10,14 +10,14 @@ import java.util.List;
 
 @Repository
 public interface ClienteRepository  extends JpaRepository<Cliente, Integer> {
-    @Query("SELECT (u.username, c.id, c.dni, SUM(lp.cantidad * lp.precioUnitario)) " +
-            "FROM Cliente c " +
-            "JOIN c.usuario u " +
-            "JOIN c.pedidos p " +
-            "JOIN p.lineaPedido lp " +
-            "GROUP BY u.username, c.id, c.dni " +
-            "ORDER BY SUM(lp.cantidad * lp.precioUnitario) DESC")
-    List<Cliente> findTotalPedidosPorCliente();
+//    @Query("SELECT (u.username, c.id, c.dni, SUM(lp.cantidad * lp.precioUnitario)) " +
+//            "FROM Cliente c " +
+//            "JOIN c.usuario u " +
+//            "JOIN c.pedidos p " +
+//            "JOIN p.lineaPedido lp " +
+//            "GROUP BY u.username, c.id, c.dni " +
+//            "ORDER BY SUM(lp.cantidad * lp.precioUnitario) DESC")
+//    List<Cliente> findTotalPedidosPorCliente();
 
     Cliente findClienteByUsuarioId(Integer id);
 }
