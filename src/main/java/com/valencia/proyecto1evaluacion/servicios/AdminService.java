e42c2db9c8df79a9891cb9f00d9b62f9a98c0d6c
davidmato <dmatodelalozapalomo@safareyes.es>
2024-12-02 13:43:15 +0100

    creacion del metodo editar proveedor

--- src/main/java/com/valencia/proyecto1evaluacion/servicios/AdminService.java
+++ src/main/java/com/valencia/proyecto1evaluacion/servicios/AdminService.java
@@ -1,11 +1,15 @@
 package com.valencia.proyecto1evaluacion.servicios;
 
 import com.valencia.proyecto1evaluacion.dtos.AuthenticationDTO;
+import com.valencia.proyecto1evaluacion.dtos.OngDTO;
+import com.valencia.proyecto1evaluacion.dtos.ProveedoresDTO;
 import com.valencia.proyecto1evaluacion.dtos.UsuarioDto;
 import com.valencia.proyecto1evaluacion.enums.Rol;
 import com.valencia.proyecto1evaluacion.modelos.Ong;
+import com.valencia.proyecto1evaluacion.modelos.Proveedores;
 import com.valencia.proyecto1evaluacion.modelos.Usuario;
 import com.valencia.proyecto1evaluacion.repositorio.OngRepository;
+import com.valencia.proyecto1evaluacion.repositorio.ProveedoresRepository;
 import com.valencia.proyecto1evaluacion.repositorio.UsuarioRepository;
 import com.valencia.proyecto1evaluacion.security.JwtService;
 import lombok.AllArgsConstructor;
@@ -23,6 +27,7 @@ public class AdminService {
     private final JwtService jwtService;
     private final OngRepository ongRepositorio;
     private final UsuarioService usuarioService;
+    private final ProveedoresRepository proveedoresRepositorio;
 
     /**
      * Registra un nuevo administrador
@@ -64,9 +69,8 @@ public class AdminService {
     /**
      * Edita una ONG siendo administrador
      * @param ongId
-     * @param updatedOng
      */
-    public  Ong editarOng(Integer ongId, Ong updatedOng) {
+    public OngDTO editarOng(Integer ongId, OngDTO ongDto) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String nombre = authentication.getName();
         Usuario usuario = usuarioService.buscarUsuarioPorNombre(nombre);
@@ -75,17 +79,118 @@ public class AdminService {
             throw new SecurityException("No tienes permiso para editar ONGs");
         }
 
-        Ong ong = ongRepositorio.findById(ongId)
+        Ong entity = ongRepositorio.findById(ongId)
                 .orElseThrow(() -> new RuntimeException("ONG no encontrada"));
 
-        ong.setNumVoluntarios(updatedOng.getNumVoluntarios());
-        ong.setSede(updatedOng.getSede());
-        ong.setDescripcion(updatedOng.getDescripcion());
-        ong.setUbicacion(updatedOng.getUbicacion());
-        ong.setImg(updatedOng.getImg());
+        entity.setNumVoluntarios(ongDto.getNumVoluntarios());
+        entity.setSede(ongDto.getSede());
+        entity.setDescripcion(ongDto.getDescripcion());
+        entity.setUbicacion(ongDto.getUbicacion());
+        entity.setImg(ongDto.getImg());
 
-        return ongRepositorio.save(ong);
+        Usuario usuarioOng = usuarioRepositorio.findById(entity.getUsuario().getId())
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+        usuarioOng.setEmail(ongDto.getEmail());
+        usuarioOng.setUsername(ongDto.getUsername());
+        if (ongDto.getPassword() != null && !ongDto.getPassword().isEmpty()) {
+            usuarioOng.setPassword(passwordEncoder.encode(ongDto.getPassword()));
+        }
+        usuarioOng.setRol(Rol.ONG);
+        Usuario savedUsuario = usuarioRepositorio.save(usuarioOng);
+        entity.setUsuario(savedUsuario);
+
+        Ong savedOng = ongRepositorio.save(entity);
+
+        OngDTO resultDto = new OngDTO();
+        resultDto.setId(savedOng.getId());
+        resultDto.setNumVoluntarios(savedOng.getNumVoluntarios());
+        resultDto.setSede(savedOng.getSede());
+        resultDto.setDescripcion(savedOng.getDescripcion());
+        resultDto.setUbicacion(savedOng.getUbicacion());
+        resultDto.setImg(savedOng.getImg());
+        resultDto.setIdUsuario(savedOng.getUsuario().getId());
+        resultDto.setEmail(savedOng.getUsuario().getEmail());
+        resultDto.setUsername(savedOng.getUsuario().getUsername());
+
+        return resultDto;
     }
+    /**
+     * Registra un nuevo proveedor siendo administrador
+     * @param proveedorDto
+     * @return
+     */
+    public ProveedoresDTO editarProveedor(Integer id, ProveedoresDTO proveedorDto) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String nombre = authentication.getName();
+        Usuario usuario = usuarioService.buscarUsuarioPorNombre(nombre);
+
+        if (usuario == null || !usuario.getRol().equals(Rol.ADMIN)) {
+            throw new SecurityException("No tienes permiso para editar proveedores");
+        }
+
+        Proveedores proveedor = proveedoresRepositorio.findById(id)
+                .orElseThrow(() -> new RuntimeException("Proveedor no encontrado"));
+
+        proveedor.setNombre(proveedorDto.getNombre());
+        proveedor.setValidado(proveedorDto.getValidado());
+        proveedor.setCif(proveedorDto.getCif());
+        proveedor.setNumVoluntarios(proveedorDto.getNumVoluntarios());
+        proveedor.setSede(proveedorDto.getSede());
+        proveedor.setUbicacion(proveedorDto.getUbicacion());
+        proveedor.setImg(proveedorDto.getImg());
+
+        Usuario usuarioProveedor = usuarioRepositorio.findById(proveedor.getUsuario().getId())
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+        usuarioProveedor.setEmail(proveedorDto.getEmail());
+        usuarioProveedor.setUsername(proveedorDto.getUsername());
+        if (proveedorDto.getPassword() != null && !proveedorDto.getPassword().isEmpty()) {
+            usuarioProveedor.setPassword(passwordEncoder.encode(proveedorDto.getPassword()));
+        }
+        usuarioProveedor.setRol(Rol.PROVEEDOR);
+        Usuario savedUsuario = usuarioRepositorio.save(usuarioProveedor);
+        proveedor.setUsuario(savedUsuario);
+
+
+        Proveedores updatedProveedor = proveedoresRepositorio.save(proveedor);
+
+        ProveedoresDTO resultDto = new ProveedoresDTO();
+        resultDto.setId(updatedProveedor.getId());
+        resultDto.setNombre(updatedProveedor.getNombre());
+        resultDto.setValidado(updatedProveedor.getValidado());
+        resultDto.setCif(updatedProveedor.getCif());
+        resultDto.setNumVoluntarios(updatedProveedor.getNumVoluntarios());
+        resultDto.setSede(updatedProveedor.getSede());
+        resultDto.setUbicacion(updatedProveedor.getUbicacion());
+        resultDto.setImg(updatedProveedor.getImg());
+        resultDto.setUsername(updatedProveedor.getUsuario().getUsername());
+        resultDto.setPassword(updatedProveedor.getUsuario().getPassword());
+        resultDto.setEmail(updatedProveedor.getUsuario().getEmail());
+
+        return resultDto;
+    }
+
+    public void eliminarProveedor(Integer id) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String nombre = authentication.getName();
+        Usuario usuario = usuarioService.buscarUsuarioPorNombre(nombre);
+
+        if (usuario == null || !usuario.getRol().equals(Rol.ADMIN)) {
+            throw new SecurityException("No tienes permiso para eliminar proveedores");
+        }
+
+        Proveedores proveedor = proveedoresRepositorio.findById(id)
+                .orElseThrow(() -> new RuntimeException("Proveedor no encontrado"));
+
+        proveedoresRepositorio.delete(proveedor);
+    }
+
+
+
+
+    /**
+     * crear proveedores siendo administrador
+     */
+
 
 
     /**
