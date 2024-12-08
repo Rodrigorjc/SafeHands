38b836e8f7f684600ef58699af695455aa303442
Rodrigo <rjaencobos@safareyes.es>
2024-12-08 12:39:26 +0100

    Se ha borrado referencia a public innecesaria.

--- src/main/java/com/valencia/proyecto1evaluacion/repositorio/ProveedoresAcontecimientoRepository.java
+++ src/main/java/com/valencia/proyecto1evaluacion/repositorio/ProveedoresAcontecimientoRepository.java
@@ -2,8 +2,6 @@ package com.valencia.proyecto1evaluacion.repositorio;
 
 import com.valencia.proyecto1evaluacion.modelos.Acontecimiento;
 import com.valencia.proyecto1evaluacion.modelos.Producto;
-import com.valencia.proyecto1evaluacion.dtos.ProductoDTO;
-import com.valencia.proyecto1evaluacion.modelos.Producto;
 import com.valencia.proyecto1evaluacion.modelos.ProveedoresAcontecimiento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,8 +14,11 @@ import java.util.List;
 public interface ProveedoresAcontecimientoRepository  extends JpaRepository<ProveedoresAcontecimiento, Integer> {
     boolean existsByProductoAndAcontecimiento(Producto producto, Acontecimiento acontecimiento);
 
-    public List<ProveedoresAcontecimiento> findByAcontecimientoId(Integer idAcontecimiento);
+    List<ProveedoresAcontecimiento> findByAcontecimientoId(Integer idAcontecimiento);
 
     @Query("SELECT pa.producto FROM ProveedoresAcontecimiento pa WHERE pa.acontecimiento.id = :acontecimientoId")
     List<Producto> findProductosByAcontecimientoId(@Param("acontecimientoId") Integer acontecimientoId);
+
+
+
 }
