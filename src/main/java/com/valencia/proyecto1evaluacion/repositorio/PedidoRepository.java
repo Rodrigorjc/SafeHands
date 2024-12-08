eccc5a96c4c9a611e407aa051b06bc83e2370808
Rodrigo <rjaencobos@safareyes.es>
2024-12-08 12:37:14 +0100

    Consulta no usada borrada.

--- src/main/java/com/valencia/proyecto1evaluacion/repositorio/PedidoRepository.java
+++ src/main/java/com/valencia/proyecto1evaluacion/repositorio/PedidoRepository.java
@@ -7,14 +7,10 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
 
-//    @Query("SELECT (p.id, SUM(lp.cantidad * lp.precioUnitario)) " +
-//            "FROM Pedido p " +
-//            "JOIN p.lineaPedido lp " +
-//            "GROUP BY p.id " +
-//            "ORDER BY SUM(lp.cantidad * lp.precioUnitario) DESC")
-//    List<Pedido> findTotalPedidos();
+
 
 }
