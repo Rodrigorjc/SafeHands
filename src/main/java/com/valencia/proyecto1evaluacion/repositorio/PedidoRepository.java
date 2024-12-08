4a9aa07bdd4efe4e7ae49b5e766e8feb5284c5be
Rodrigo <rjaencobos@safareyes.es>
2024-12-08 12:40:00 +0100

    Se ha borrado referencias no utilizadas.

--- src/main/java/com/valencia/proyecto1evaluacion/repositorio/PedidoRepository.java
+++ src/main/java/com/valencia/proyecto1evaluacion/repositorio/PedidoRepository.java
@@ -2,10 +2,8 @@ package com.valencia.proyecto1evaluacion.repositorio;
 
 import com.valencia.proyecto1evaluacion.modelos.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 
 @Repository
