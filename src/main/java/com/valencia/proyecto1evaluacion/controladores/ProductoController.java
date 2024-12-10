d9ed0c380111a7e3f288f74cfe895159e9d4e98c
davidmato <dmatodelalozapalomo@safareyes.es>
2024-12-10 18:38:25 +0100

    eliminacion de codigo no utilizado

--- src/main/java/com/valencia/proyecto1evaluacion/controladores/ProductoController.java
+++ src/main/java/com/valencia/proyecto1evaluacion/controladores/ProductoController.java
@@ -23,7 +23,6 @@ public class    ProductoController {
         return productoService.getAll();
     }
 
-    //cambiar para buscar por ususario
     @GetMapping("/listar/{proveedorId}")
     public List<ProductoDTO> listarProductosPorProveedor(@PathVariable Integer proveedorId) {
         return productoService.getProductosByProveedorId(proveedorId);
@@ -47,21 +46,6 @@ public class    ProductoController {
     }
 
 
-    // Endpoint con filtros para buscar productos por precio, proveedor y nombre
-//    @GetMapping("/buscar")
-//    public ResponseEntity<List<Producto>> buscarProductos(
-//            @RequestParam(value = "precioMin", required = false) Double precioMin,
-//            @RequestParam(value = "precioMax", required = false) Double precioMax,
-//            @RequestParam(value = "proveedor", required = false) Integer proveedor,
-//            @RequestParam(value = "nombre", required = false) String nombre) {
-//
-//        // Llamar al servicio para obtener los productos filtrados
-//        List<Producto> productos = productoService.buscarProductos(precioMin, precioMax, proveedor, nombre);
-//
-//        return ResponseEntity.ok(productos);
-//    }
-
-
 
 
     @PostMapping("/{productoId}/vincular/{acontecimientoId}")
@@ -69,11 +53,6 @@ public class    ProductoController {
         return productoService.vincularProductoAcontecimiento(productoId, acontecimientoId);
     }
 
-
-//    @GetMapping("/acontecimiento")
-//    public List<ProductoDTO> obtenerProductosPorAcontecimiento(@RequestParam Integer idAcontecimiento) {
-//        return productoService.getProductosByAcontecimientoId(idAcontecimiento);
-//    }
 
 
     @GetMapping("/acontecimiento/{idAcontecimiento}")
